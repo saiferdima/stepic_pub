2fe7a7694aa89f62486e6ae331b98ee80beded5e
Dmitry <saiferdima@gmail.com>
2019-10-03 15:00:49 +0300

    Updated test to assert button displayed

--- test_items.py
+++ test_items.py
@@ -1,7 +1,9 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_cart_button_is_displayed(browser):
     browser.get(link)
     add_to_basket_button= browser.find_element_by_xpath("//div[@class = 'row']//button[@type= 'submit']")
     assert add_to_basket_button.is_displayed(), "add_to_basket_button  Not Displayed"
+
+
